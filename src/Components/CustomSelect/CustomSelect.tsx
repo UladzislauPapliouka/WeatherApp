dacf3616b23f8f6f7824441079273ada3957138a
Uladzislau Papliouka <uladzislau.papliouka@gmail.com>
2023-01-27 21:14:28 +0300

    Added CustomSlect component

---
+++ src/Components/CustomSelect/CustomSelect.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import styles from './CustomSelect.module.scss';
+
+interface ICustomSelect {
+  options: string[],
+  selected: string,
+  onChangeSelected: (value: string) => void
+}
+
+export default function CustomSelect({
+  options, selected, onChangeSelected,
+}: ICustomSelect) {
+  const [isActive, setIsActive] = useState<boolean>(false);
+  const toggleSelect = () => setIsActive(!isActive);
+  const onChangeSelectedHandler = (value: string) => {
+    toggleSelect();
+    onChangeSelected(value);
+  };
+  // TODO: maybe change styles and elsint error fix correct
+  return (
+    <div
+      tabIndex={0}
+      onKeyPress={(e) => e.key}
+      role="button"
+      className={styles.customSelect}
+      onClick={() => toggleSelect()}
+    >
+      <div>
+        <span>{selected}</span>
+        <div className={`${styles.arrow} ${isActive && styles.activeArrow}`} />
+      </div>
+      {
+                isActive
+                && (
+                <div
+                  tabIndex={-1}
+                  onKeyPress={(e) => e.key}
+                  role="button"
+                  onClick={(event) => {
+                    event.stopPropagation();
+                  }}
+                  className={styles.dropdownMenu}
+                >
+                  {options.map((opt, i) => (
+                    <span
+                      tabIndex={-i - 2}
+                      onKeyPress={(e) => e.key}
+                      role="button"
+                      key={opt}
+                      onClick={() => onChangeSelectedHandler(opt)}
+                    >
+                      {opt}
+                    </span>
+                  ))}
+                </div>
+                )
+            }
+    </div>
+  );
+}
