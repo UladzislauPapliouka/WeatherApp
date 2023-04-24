93322d0e04983ac22490b365d1d7e9a87f848b5f
Uladzislau Papliouka <uladzislau.papliouka@gmail.com>
2023-04-24 12:51:38 +0300

    refactor: refactor GlobalStyles

--- src/components/GlobalStyles/index.ts
+++ src/components/GlobalStyles/index.ts
@@ -3,7 +3,6 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyles = createGlobalStyle`
   body {
   margin: 0;
-  font-family: 'New Rocker', cursive;
   font-family: 'Vazirmatn', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
