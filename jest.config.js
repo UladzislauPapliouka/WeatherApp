93daa74035cf5b5c8713a94e2608c539cefdcb36
Uladzislau Papliouka <uladzislau.papliouka@gmail.com>
2023-04-26 09:49:39 +0300

    fix: fix test aliases paths

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-    '^@constants/(.*)$': '<rootDir>/src/constant/$1',
+    '^@constants/(.*)$': '<rootDir>/src/constants/$1',
     '^@assets/(.*)$': '<rootDir>/src/assets/$1',
     '^@components/(.*)$': '<rootDir>/src/components/$1',
     '^containers/(.*)$': '<rootDir>/src/containers/$1',
@@ -9,6 +9,8 @@ module.exports = {
     '^@Types/(.*)$': '<rootDir>/src/types/$1',
     '^@store/(.*)$': '<rootDir>/src/store/$1',
   },
+  preset: 'ts-jest',
+  resolver: 'ts-jest-resolver',
   transform: {
     '\\.[jt]sx?$': 'babel-jest',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
